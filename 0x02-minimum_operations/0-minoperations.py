09b36f6f6f3b186d1dede35083dca7898eacd8b6
Ebi-Tech <difebi14@gmail.com>
2023-04-05 12:07:10 +0100

    Update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,44 +1,60 @@
 #!/usr/bin/python3
+'''Minimum Operations python3 challenge'''
+
 
 def minOperations(n):
-    """
-    Calculates the minimum number of operations needed
-    to achieve 'n' H characters in a text file.
+    '''calculates the fewest number of
+    operations needed to result in exactly n H
+    characters in this file.
+    Returns: Integer
+        if n is impossible to achieve, return 0
+    '''
+    pasted_chars = 1  # how many chars in the file
+    clipboard = 0  # how many H's copied
+    counter = 0  # operations counter
 
-    Args:
-        n (int): the number of H characters to achieve
+    while pasted_chars < n:
+        # if the clipboard is empty, copy all the characters in the file
+        if clipboard == 0:
+            # copyall
+            clipboard = pasted_chars
+            # increment the operations counter
+            counter += 1
 
-    Returns:
-        int: the minimum number of operations needed
-        to achieve 'n' H characters in a text file
-    """
+        # if nothing has been pasted yet
+        if pasted_chars == 1:
+            # paste
+            pasted_chars += clipboard
+            # increment the operations counter
+            counter += 1
+            # continue to the next loop
+            continue
 
-    # Base case: If n is 1, the file already contains
-    # 1 H character, so 0 operations are needed
-    if n == 1:
-        return 0
+        remaining = n - pasted_chars  # remaining chars to Paste
+        # check if is impossible by checking if the clipboard
+        # has more than what is required to reach the number
+        # which also means num of chars in file is equal
+        # or more than in the clipboard.
+        # in either situations, it's impossible to achieve n of chars
+        if remaining < clipboard:
+            return 0
 
-    # Create an array dp to store the minimum number of operations
-    # required to achieve each possible number of H characters.
-    # Initialize the array with infinity for all states except the base case.
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
+        # if can't be devided
+        if remaining % pasted_chars != 0:
+            # paste current clipboard
+            pasted_chars += clipboard
+            # increment the operations counter
+            counter += 1
+        else:
+            # copyall
+            clipboard = pasted_chars
+            # paste
+            pasted_chars += clipboard
+            # increment the operations counter
+            counter += 2
 
-    # Iterate over all possible states from 2 to n
-    for i in range(2, n + 1):
-        # For each state, try all possible previous states j
-        for j in range(1, i):
-            # Check if j is a factor of i, meaning j can be combined to form i
-            if i % j == 0:
-                # Calculate the number of operations needed to achieve state i
-                # by combining state j with (i // j) copies of state j
-                operations = dp[j] + i // j
-                # Update dp[i] if a smaller number of operations is found
-                dp[i] = min(dp[i], operations)
-
-                # Return the minimum number of operations required
-                # to achieve the state with 'n' characters in the file
-    if dp[n] == float('inf'):
-        return 0
+    # if got the desired result
+    if pasted_chars == n:
+        return counter
     else:
-        return dp[n]
+        return 0
