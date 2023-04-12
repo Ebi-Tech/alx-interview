0bfba57e5ed34ab450203b679587972398981249
Ebi-Tech <difebi14@gmail.com>
2023-04-12 15:38:11 +0100

    Update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 '''This script reads stdin line by line and computes metrics'''
 
+
 import sys
 import signal
+
 
 # Define the signal handler for CTRL + C
 def signal_handler(sig, frame):
     print_metrics()
     sys.exit(0)
+
 
 # Define a dictionary to store the count of each status code
 status_code_count = {}
@@ -15,14 +18,17 @@ status_code_count = {}
 # Define a variable to store the total file size
 total_file_size = 0
 
+
 # Define a function to print the metrics
 def print_metrics():
     print(f"Total file size: {total_file_size}")
     for status_code in sorted(status_code_count.keys()):
         print(f"{status_code}: {status_code_count[status_code]}")
 
+
 # Set the signal handler for CTRL + C
 signal.signal(signal.SIGINT, signal_handler)
+
 
 # Read stdin line by line
 line_count = 0
@@ -47,6 +53,6 @@ for line in sys.stdin:
         if line_count % 10 == 0:
             print_metrics()
 
-    except:
+    except Exception as err:
         # Skip lines that don't match the input format
         continue
