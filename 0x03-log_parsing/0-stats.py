e50a383ba117177779bbd565dca692698907e303
Ebi-Tech <difebi14@gmail.com>
2023-04-12 15:47:28 +0100

    Update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,37 @@
 #!/usr/bin/python3
-'''This script reads stdin line by line and computes metrics'''
+'''a script that reads stdin line by line and computes metrics'''
 
 
 import sys
-import signal
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-# Define the signal handler for CTRL + C
-def signal_handler(sig, frame):
-    print_metrics()
-    sys.exit(0)
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-# Define a dictionary to store the count of each status code
-status_code_count = {}
+except Exception as err:
+    pass
 
-# Define a variable to store the total file size
-total_file_size = 0
-
-
-# Define a function to print the metrics
-def print_metrics():
-    print(f"Total file size: {total_file_size}")
-    for status_code in sorted(status_code_count.keys()):
-        print(f"{status_code}: {status_code_count[status_code]}")
-
-
-# Set the signal handler for CTRL + C
-signal.signal(signal.SIGINT, signal_handler)
-
-
-# Read stdin line by line
-line_count = 0
-for line in sys.stdin:
-    line_count += 1
-    try:
-        # Parse the line and extract the file size and status code
-        parts = line.split()
-        file_size = int(parts[-1])
-        status_code = int(parts[-2])
-
-        # Update the total file size
-        total_file_size += file_size
-
-        # Update the status code count
-        if status_code in status_code_count:
-            status_code_count[status_code] += 1
-        else:
-            status_code_count[status_code] = 1
-
-        # Print the metrics every 10 lines
-        if line_count % 10 == 0:
-            print_metrics()
-
-    except Exception as err:
-        # Skip lines that don't match the input format
-        continue
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
