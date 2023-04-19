990ed5320dc473f8594cc6842e3359e165c00889
Ebi-Tech <difebi14@gmail.com>
2023-04-19 21:42:53 +0100

    Update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,6 +6,16 @@ UTF-8 Validation
 
 
 def validUTF8(data):
+    """
+    Determine if a given data set represents a valid UTF-8 encoding.
+    Args:
+    data (list of integers):
+    The data set to be checked for a valid UTF-8 encoding.
+    Each integer represents 1 byte of data, so only the 8 least
+    significant bits of each integer are considered.
+    Returns:
+    bool: True if data is a valid UTF-8 encoding, else False.
+    """
     num_bytes = 0
     for byte in data:
         # Check if current byte is a start byte
@@ -18,9 +28,9 @@ def validUTF8(data):
                 num_bytes = 3
             elif (byte >> 7):
                 return False
-        else:
-            # Check if current byte is a continuation byte
-            if (byte >> 6) != 0b10:
-                return False
-            num_bytes -= 1
-    return num_bytes == 0
+            else:
+                # Check if current byte is a continuation byte
+                if (byte >> 6) != 0b10:
+                    return False
+                num_bytes -= 1
+                return num_bytes == 0
