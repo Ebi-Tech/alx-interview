cf1316211fd6fe4b2317632cc24b58ad3e68de73
Ebi-Tech <difebi14@gmail.com>
2023-05-23 10:05:29 +0100

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,6 +5,33 @@ island_perimeter
 
 
 def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in the grid.
+
+    Args:
+        grid (list): A list of lists representing the grid.
+                     0 represents water, and 1 represents land.
+                     The grid is rectangular with width and
+                     height not exceeding 100.
+
+    Returns:
+        int: The perimeter of the island.
+
+    Raises:
+        ValueError: If the grid is empty or not rectangular.
+
+    Notes:
+        - Cells are connected horizontally/vertically but not diagonally.
+        - The grid is completely surrounded by water.
+        - There is only one island (or nothing).
+        - The island doesn't have "lakes" (water inside that isn't
+        connected to the water surrounding the island).
+    """
+
+    if not grid or not all(len(row) == len(grid[0]) for row in grid):
+        raise ValueError("Invalid grid: The grid must be"
+                         "non-empty and rectangular.")
+
     rows = len(grid)
     cols = len(grid[0])
     perimeter = 0
